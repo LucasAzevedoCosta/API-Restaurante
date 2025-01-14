a4c1444b7322270bbeb328766148cf6f62d3c170
Lucas Azevedo Costa <eozecos@gmail.com>
2025-01-14 09:07:21 -0300

    Autenticação pelo link parte:1

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,22 +1,16 @@
-import { Elysia, t } from "elysia";
+import { Elysia } from "elysia";
+
+
 import { registerRestaurant } from "./routes/register-restaurant";
 import { sendAuthLink } from "./routes/send-auth-link";
-import { env } from "../env";
-import jwt from "@elysiajs/jwt";
-import cookie from "@elysiajs/cookie";
+import { authenticateFromLink } from "./routes/authenticate-from-link";
 
-const app = new Elysia().use(
-    jwt({
-      secret: env.JWT_SECRET_KEY,
-      schema: t.Object({
-        sub: t.String(),
-        restauranteId: t.Optional(t.String()),
-      }),
-    }),
-  )
-  .use(cookie())
+
+
+const app = new Elysia()
   .use(registerRestaurant)
   .use(sendAuthLink)
+  .use(authenticateFromLink)
 
 app.listen(3333, () => {
     console.log("HTTP está rodando")
