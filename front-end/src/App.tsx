da7b1be6eee0a93c07a9be782b0ba22815005503
LucasAzevedoCosta <eozecos@gmail.com>
2025-03-26 23:13:48 -0300

    :construction: Feat: implementando Notificações toast (sonner)

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
+import { Toaster } from 'sonner'
  
 import { router } from './routes'
 
 export function App() {
   return <>
   <Helmet titleTemplate="%s | Lucas Food" />
+
+  <Toaster richColors />
 
   <RouterProvider router={router} />
   </>
