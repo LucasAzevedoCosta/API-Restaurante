fda04c29bcfb171da10ca2cd0ea9d404889d1e86
LucasAzevedoCosta <eozecos@gmail.com>
2025-04-08 23:43:48 -0300

    :bug: Fix: Erro auth/authenticate

--- api/src/http/auth.ts
+++ api/src/http/auth.ts
@@ -1,65 +1,77 @@
-import  { Elysia, t, type Static } from "elysia";
+import { Elysia, t, type Static } from "elysia";
 import jwt from "@elysiajs/jwt";
 
 import { env } from "../env";
 import { UnauthorizedError } from "./errors/unauthorized-error";
+import { NotAManagerError } from "./errors/not-a-manager-error";
 
 const jwtPayload = t.Object({
   sub: t.String(),
   restauranteId: t.Optional(t.String()),
-})
+});
 
 export const auth = new Elysia()
-.error({
-  UNAUTHORIZED: UnauthorizedError,
-})
-.onError(({ error, code, set }) => {
-  switch (code) {
-    case 'UNAUTHORIZED': {
-      set.status = 401
-      return {
-        code,
-        message: error.message,
-      }
+  .error({
+    UNAUTHORIZED: UnauthorizedError,
+    NOT_A_MANAGER: NotAManagerError,
+  })
+  .onError(({ error, code, set }) => {
+    switch (code) {
+      case "UNAUTHORIZED":
+        set.status = 401;
+        return {
+          code,
+          message: error.message,
+        };
+      case "NOT_A_MANAGER":
+        set.status = 401;
+        return { code, message: error.message };
     }
-  }
-})
-.use(
-  jwt({
-    secret: env.JWT_SECRET_KEY,
-    schema: jwtPayload,
-  }),
-)  
-.derive({ as: 'scoped' }, ({ jwt, cookie: { auth }, cookie }) => {
-  return {
-    signUser: async (payload: Static<typeof jwtPayload>) => {
-      const token = await jwt.sign(payload)
+  })
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: jwtPayload,
+    })
+  )
+  .derive({ as: "scoped" }, ({ jwt, cookie: { auth }, cookie }) => {
+    return {
+      getCurrentUser: async () => {
+        const payload = await jwt.verify(cookie.auth);
 
-      auth.value = token
-      auth.httpOnly = true
-      auth.maxAge = 60 * 60 * 24 * 7 // 7 days
-      auth.path = '/'
-    },
+        if (!payload) {
+          throw new UnauthorizedError();
+        }
 
-    signOut: async () => {
-      auth.remove()
-    },
+        return {
+          userId: payload.sub,
+          restauranteId: payload.restauranteId,
+        };
+      },
+      signUser: async (payload: Static<typeof jwtPayload>) => {
+        const token = await jwt.sign(payload);
 
+        auth.value = token;
+        auth.httpOnly = true;
+        auth.maxAge = 60 * 60 * 24 * 7; // 7 days
+        auth.path = "/";
+      },
 
-    getCurrentUser: async () => {
+      signOut: async () => {
+        auth.remove();
+      },
+    };
+  })
+  .derive({ as: "scoped" }, ({ getCurrentUser }) => {
+    return {
+      getManagedRestaurantId: async () => {
+        const { restauranteId } = await getCurrentUser();
 
-      const payload = await jwt.verify(cookie.auth)
+        if (!restauranteId) {
+          throw new NotAManagerError();
+        }
 
-
-      if (!payload) {
-        throw new UnauthorizedError()
-      }
-
-
-      return {
-        userId: payload.sub,
-        restauranteId: payload.restauranteId,
-      }
-    },
-  }
-})
+        return restauranteId;
+      },
+    };
+  });
--- api/src/http/routes/send-authentication-link.ts
+++ api/src/http/routes/send-authentication-link.ts
@@ -3,12 +3,13 @@ import { db } from "../../db/connection";
 import { createId } from "@paralleldrive/cuid2";
 import { authLinks } from "../../db/schema";
 import { env } from "../../env";
-import { AuthenticationMagicLinkTemplate } from "../../mail/templates/authentication-magic-link";
-import { resend } from "../../mail/client";
+//import { AuthenticationMagicLinkTemplate } from "../../mail/templates/authentication-magic-link";
+//import { resend } from "../../mail/client";
+import { UnauthorizedError } from "../errors/unauthorized-error";
 
 export const sendAuthenticationLink = new Elysia().post(
   "/authenticate",
-  async ({ body, set }) => {
+  async ({ body }) => {
     const { email } = body;
 
     const userFromEmail = await db.query.users.findFirst({
@@ -18,9 +19,7 @@ export const sendAuthenticationLink = new Elysia().post(
     });
 
     if (!userFromEmail) {
-      set.status = 401;
-
-      throw new Error("Unauthorized");
+      throw new UnauthorizedError();
     }
 
     const authLinkCode = createId();
@@ -32,17 +31,19 @@ export const sendAuthenticationLink = new Elysia().post(
 
     const authLink = new URL("/auth-links/authenticate", env.API_BASE_URL);
     authLink.searchParams.set("code", authLinkCode);
-    authLink.searchParams.set("redirect", env.API_BASE_URL);
+    authLink.searchParams.set("redirect", env.AUTH_REDIRECT_URL);
 
-    await resend.emails.send({
-      from: "Lucas's Food <naoresponda@fala.dev>",
-      to: email,
-      subject: "[Lucas's Food] Link para login",
-      react: AuthenticationMagicLinkTemplate({
-        userEmail: email,
-        authLink: authLink.toString(),
-      }),
-    });
+    console.log(authLink.toString());
+
+    // await resend.emails.send({
+    //   from: 'Lucas's Food <naoresponda@fala.dev>',
+    //   to: email,
+    //   subject: '[Lucas's Food] Link para login',
+    //   react: AuthenticationMagicLinkTemplate({
+    //     userEmail: email,
+    //     authLink: authLink.toString(),
+    //   }),
+    // })
   },
   {
     body: t.Object({
